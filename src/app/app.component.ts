63562a0c9e5028c45e39dd29149fca7c19a297b2
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2021-12-07 08:28:59 +0500

    fix: change "tempreature" to "temperature"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WeatherApiService } from 'src/app/services/weather-api.service';
@@ -10,7 +9,7 @@ import { WeatherDaily, WeatherResponse } from './models/weather-response.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public tempreatures: number[] = [];
+  public temperatures: number[] = [];
   subscription!: Subscription;
   constructor(private weatherApiService: WeatherApiService) {}
 
@@ -19,18 +18,15 @@ export class AppComponent implements OnInit {
       .getDataFor7Days()
       .subscribe((data: WeatherResponse) => {
         data.daily.map((day: WeatherDaily) => {
-          this.tempreatures.push(
-            this.kelvinToCelcius(day.temp.night),
-            this.kelvinToCelcius(day.temp.morn),
-            this.kelvinToCelcius(day.temp.day),
-            this.kelvinToCelcius(day.temp.eve)
+          console.log(day);
+          this.temperatures.push(
+            day.temp.night,
+            day.temp.morn,
+            day.temp.day,
+            day.temp.eve
           );
         });
       });
-  }
-
-  kelvinToCelcius(kelvin: number) {
-    return Math.round(kelvin - 273.15);
   }
 
   ngOnDestroy() {
